2a29a4b4320d57b6554a143452c71583da94fc11
manaslaud <manaslaud2004@gmail.com>
2025-02-07 23:49:16 +0530

    feat: added db config and unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod cid;
+pub mod storage;
---
+++ src/storage/init_db.rs
@@ -0,0 +1,28 @@
+use fjall::{Config, Error, Keyspace,PartitionHandle,PartitionCreateOptions};
+use std::path::PathBuf;
+use std::fs;
+
+
+pub async fn init_db(path:String) -> Result<PartitionHandle,Error> {
+    let folder: PathBuf = PathBuf::from(path); 
+    if !folder.exists() {
+        fs::create_dir_all(&folder)?;
+    }
+    let keyspace: Keyspace = Config::new(folder).open()?; 
+    let items: PartitionHandle = keyspace.open_partition("slices", PartitionCreateOptions::default())?;
+    Ok(items)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::init_db;
+
+    #[tokio::test]
+    async fn test_insert() {
+        let items = init_db(String::from("./tmp/data")).await.unwrap();
+        let _ = items.insert("key", "value").unwrap();
+        let ret: fjall::Slice = items.get("key").unwrap().unwrap();
+        let value_from_db = String::from_utf8_lossy(ret.as_ref()).to_string();
+        assert_eq!(value_from_db, "value");
+    }
+}
