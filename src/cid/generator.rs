12b41164575487bf4143dedb16d25d9d3a6763dd
manaslaud <manaslaud2004@gmail.com>
2025-02-17 22:08:12 +0530

    fix: issues in generator.rs, unit tests added

--- src/cid/generator.rs
+++ src/cid/generator.rs
@@ -1,9 +1,25 @@
 use cid::Cid;
 use multihash::Multihash;
 
-const SHA2_256: u64 = 0x12;
-
 pub fn generate_cid(data: &[u8]) -> Cid {
-	let hash = Multihash::<64>::wrap(SHA2_256, data).expect("Could not hash slice");
+    const SHA2_256: u64 = 0x12;
+    //create the digest from data
+    let hash = Multihash::<64>::wrap(SHA2_256, data).expect("Could not hash slice");
     Cid::new_v1(0x55, hash)
+}
+
+//tests for the generate_cid function
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use multihash::Multihash;
+    #[test]
+    fn test_generate_cid() {
+        const SHA2_256: u64 = 0x12;
+        let data: [u8; 8] = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+        let hash: Multihash<64> =
+            Multihash::<64>::wrap(SHA2_256, &data).expect("Could not hash slice");
+        let cid = Cid::new_v1(0x55, hash);
+        assert_eq!(generate_cid(&data), cid);
+    }
 }
