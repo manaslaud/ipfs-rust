03fd4dc94c5ba2a617beeb208235736b2292d3b8
Manas Laud <manaslaud2004@gmail.com>
2025-03-30 18:49:26 +0530

    feat:added missing deps

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use actix_web::{App, HttpServer};
 use ipfs_rust::constants::constants::_PORT;
-use ipfs_rust::network::http_gateway::upload_file::upload_file;
+// use ipfs_rust::network::http_gateway::upload_file::upload_file;
 #[actix_web::main]
 pub async fn main()  {
     //defining and spinning up the http server
-    let server = HttpServer::new(|| App::new().service(upload_file))
+    let server = HttpServer::new(|| App::new())
         .bind(("127.0.0.1", _PORT));
     match server {
         Ok(server) => {
--- src/network/http_gateway/mod.rs
+++ src/network/http_gateway/mod.rs
@@ -1,2 +1,1 @@
-pub mod upload_file;
 pub mod health;
