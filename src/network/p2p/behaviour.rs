3fa7557517c3a343ce38d41f03f150484602877a
Manas Laud <manaslaud2004@gmail.com>
2025-05-01 22:13:29 +0530

    feat:basic behaviour

--- src/network/p2p/behaviour.rs
+++ src/network/p2p/behaviour.rs
@@ -1,17 +1,28 @@
-use libp2p::kad::Event as KadEvent;
-use libp2p::kad::{store::MemoryStore, Behaviour,};
+use super::{Request, Response};
 use libp2p::identify::{Behaviour as IdentifyBehaviour, Event as IdentifyEvent};
+use libp2p::kad::{store::MemoryStore, Behaviour as KademliaBehaviour, Event as KadEvent};
+use libp2p::request_response::json::Behaviour as RequestResponseJsonBehaviour;
+use libp2p::request_response::{
+    Behaviour as RequestResponseBehaviour,
+    Event as RequestResponseEvent,
+    ProtocolSupport,
+};
 use libp2p::swarm::NetworkBehaviour;
+
+
 #[derive(NetworkBehaviour)]
 #[behaviour(to_swarm = "AgentEvent")]
 pub struct AgentBehavior {
-    pub kad: Behaviour<MemoryStore>,
+    pub kad: KademliaBehaviour<MemoryStore>,
     pub identify: IdentifyBehaviour,
+    pub rr: RequestResponseJsonBehaviour<Request, Response>,
 }
+
 #[derive(Debug)]
 pub enum AgentEvent {
     Kad(KadEvent),
     Identify(IdentifyEvent),
+    RequestResponse(RequestResponseEvent<Request, Response>),
 }
 
 impl From<KadEvent> for AgentEvent {
@@ -19,8 +30,25 @@ impl From<KadEvent> for AgentEvent {
         AgentEvent::Kad(event)
     }
 }
+
 impl From<IdentifyEvent> for AgentEvent {
     fn from(event: IdentifyEvent) -> Self {
         AgentEvent::Identify(event)
+    }
+}
+
+impl From<RequestResponseEvent<Request, Response>> for AgentEvent {
+    fn from(event: RequestResponseEvent<Request, Response>) -> Self {
+        AgentEvent::RequestResponse(event)
+    }
+}
+
+impl AgentBehavior {
+    pub fn new(kad: KademliaBehaviour<MemoryStore>, identify: IdentifyBehaviour, rr: RequestResponseJsonBehaviour<Request, Response>) -> Self {
+        AgentBehavior {
+            kad,
+            identify,
+            rr,
+        }
     }
 }
